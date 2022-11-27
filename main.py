b6df072787a97d715a4d3da46d9138f96725613f
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-11-27 23:41:33 +0300

    main update on client side

--- main.py
+++ main.py
@@ -1,54 +1,139 @@
-from algorithms import bf, dc, kd
+from    algorithms  import bf, dc, kd
+from    utils       import generateTestArray
+from    utils       import safeStart, safeStop
+import  argparse
+import  timeit
+import pandas as pd
+import matplotlib.pyplot as plt
 
-import random
+# Method that simulates the algorithms. For given repeat times it repeats and measures the average time of n repeats.
+# @param int N: The number of elements in the array.
+# @param int BORDER: The borders of the elements in the array.
+# @param int REPEAT: The number of repeats.
+# @return dict: The results of the simulation.
+def simulate(N, BORDER, REPEAT) :
 
-import timeit
-import argparse
+    testArray = generateTestArray(N, BORDER)
 
-def generateTestArray(N, border=100) :
+    data = []
 
-    testArray = [0] * N
+    for i in range(REPEAT) :
+        startTimeForBF = timeit.default_timer()
+        BFResult = bf(testArray)
+        endTimeForBF = timeit.default_timer()
+        BFTime = (round((endTimeForBF - startTimeForBF) * 10 ** 6, 3))
 
-    for i in range(0, N) :
-        testArray[i] = random.randint(-border, border)
+        startTimeForDC = timeit.default_timer()
+        DCResult = dc(testArray)
+        endTimeForDC = timeit.default_timer()
+        DCtime = (round((endTimeForDC - startTimeForDC) * 10 ** 6, 3))
 
-    print("The generated test array is as follows:")
-    print(testArray)
+        startTimeForKD = timeit.default_timer()
+        KDResult = kd(testArray)
+        endTimeForKD = timeit.default_timer()
+        KDtime = (round((endTimeForKD - startTimeForKD) * 10 ** 6, 3))
 
-    return testArray
-
-def main(N) :
-
-    testArray = generateTestArray(N)
-
-    startForBruteForce = timeit.default_timer()
-    bruteForceResult = bf(testArray)
-    endForBruteForce = timeit.default_timer()
-
-    startForDivideAndConquer = timeit.default_timer()
-    divideAndConquerResult = dc(testArray)
-    endForDivideAndConquer = timeit.default_timer()
-
-    startForKadane = timeit.default_timer()
-    kadaneResult = kd(testArray)
-    endForKadane = timeit.default_timer()
-
-    bfTime = (round((endForBruteForce - startForBruteForce) * 10 ** 6, 3))
-    dcTime = (round((endForDivideAndConquer - startForDivideAndConquer) * 10 ** 6, 3))
-    kdTime = (round((endForKadane - startForKadane) * 10 ** 6, 3))
-
-    print("\nFor the given N value of " + str(N) + " the results are as follows:")
-    print("Algorithm -> {:^20} outputs as {:^15} and elapsed time is {} µs.".format("Brute Force", str(bruteForceResult), bfTime))
-    print("Algorithm -> {:^20} outputs as {:^15} and elapsed time is {} µs.".format("Divide and Conquer", str(divideAndConquerResult), dcTime))
-    print("Algorithm -> {:^20} outputs as {:^15} and elapsed time is {} µs.".format("Kadane", str(kadaneResult), kdTime))
-
+        results = (
+            BFResult,
+            DCResult,
+            KDResult,
+            BFTime,
+            DCtime,
+            KDtime
+        )
         
+        data.append(results)
+
+    if (REPEAT == 1) :
+        return (
+            N,
+            data[0]
+        )
+    else :
+
+        for currentTuple in data :
+            for otherTuple in data :
+                if (currentTuple[:3] != otherTuple[:3]) :
+                    print("ERROR: Results are not the same!")
+                    return (
+                        N,
+                        data[0]
+                    )
+        
+        BFTime = 0
+        DCtime = 0
+        KDtime = 0
+        for currentTuple in data :
+            BFTime += currentTuple[3]
+            DCtime += currentTuple[4]
+            KDtime += currentTuple[5]
+
+        BFTime = BFTime / REPEAT
+        DCtime = DCtime / REPEAT
+        KDtime = KDtime / REPEAT
+
+        return (
+            N,
+            (
+                data[0][0],
+                data[0][1],
+                data[0][2],
+                BFTime,
+                DCtime,
+                KDtime
+            )
+        )
+
+def saveToFile(data, fileName):
+    
+    
+
+# Main Method, that runs the program according to the given arguments.
+# @param int N: The number of elements in the array.
+# @param int BORDER: The borders of the elements in the array.
+# @param bool continuouslyGenerate: If the program should generate test cases continuously.
+# @param bool saveToExcel: If the program should save the results to an excel file.
+# @param bool plotResults: If the program should plot the results.
+# @param bool generateReport: If the program should generate a report.
+# @return None
+def main(N:int, BORDER:int, REPEAT:int, continuouslyGenerate:bool, savetoFile:bool, plotResults:bool, generateReport:bool) -> None :
+
+    if not continuouslyGenerate :
+        data = [simulate(N,BORDER,REPEAT)]
+    else :
+        data = []
+        for n in range(1, N + 1) :
+            data.append(simulate(n,BORDER,REPEAT))
+
+    if savetoFile :
+        saveToFile(data, "results.xlsx")
+    else :
+        print(data[0], " ...")
+
+
+# Client Driver        
 if __name__ == "__main__":
 
+    safeStart()
+
     parser = argparse.ArgumentParser(description="This script is used to compare the performance of the brute force, divide and conquer and Kadane's algorithms.")
-    parser.add_argument("-n", "--number", type=int, help="The number of elements in the array.", required=True)
+    parser.add_argument("-n", "--number-of-elements", type=int, help="The number of elements in the array.", required=True)
+    parser.add_argument("-b", "--border", type=int, help="The borders of the elements in the array.", default=100)
+    parser.add_argument("-re", "--repeat", type=int, help="The number of times to repeat a simulation.", default=1)
+    parser.add_argument("-c", "--continuously-generate", type=int, help="If this flag is set, the script will generate test cases continuously from 1 to N.", required=False, default=False)
+    parser.add_argument("-s", "--save-to-file", type=int, help="If this flag is set, the results will be saved to a file.", required=False, default=False)
+    parser.add_argument("-p", "--plot-results", type=int, help="If this flag is set, the results will be plotted.", required=False, default=False)
+    parser.add_argument("-r", "--generate-report", type=int, help="If this flag is set, the results will be reported.", required=False, default=False)
     args = parser.parse_args()
 
-    N = args.number
+    N                       = args.number_of_elements
+    BORDER                  = args.border
+    REPEAT                  = args.repeat 
+    continuouslyGenerate    = bool(args.continuously_generate)
+    savetoFile             = bool(args.save_to_file)
+    plotResults             = bool(args.plot_results)
+    generateReport          = bool(args.generate_report)
 
-    main(N)
+    main(N, BORDER, REPEAT, continuouslyGenerate, savetoFile, plotResults, generateReport)
+
+    safeStop()
