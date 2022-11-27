063050494cbf82f9854eda5dd13758f92a45377f
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-11-27 21:00:05 +0300

    add O(N^2) algorithm as an external script

---
+++ Algorithms/brute_force.py
@@ -0,0 +1,53 @@
+"""
+@Script, that implements an own-design naive algorithm to find a contiguous subarray with the largest sum as an external method.
+
+@Owns: "Emir Cetin Memis" & "Emircan Yaprak"
+@Contiributors: "Emir Cetin Memis" & "Emircan Yaprak"
+
+@Student_1:     "Emir Cetin Memis"    |   @Student_2:     "Emircan Yaprak"
+@StudentID_1:   041901027             |   @StudentID_2:   041901009
+@Contact_1:     "memise@mef.edu.tr"   |   @Contact_2:     "yaprakem@mef.edu.tr"
+
+@Set&Rights: "MEF University"
+@Instructor: "Prof. Dr. Muhittin Gokmen"
+@Course:     "Analysis of Algorithms"
+@Req:        "Project 1"
+
+@Since: 11/27/2022
+"""
+
+from math import inf
+
+# Method that implements the naive algorithm.
+# @param list array: The array that will be used to find the subarray with the largest sum.
+# @return tuple: The tuple that contains the start index, end index and the sum of the subarray with the largest sum.
+def _brute_force(array:list) -> tuple :
+
+    maximumSum = -inf
+    startIndex = 0
+    endIndex   = 0
+
+    for currentIndex in range(len(array)) :
+
+        currentSum = array[currentIndex]
+        if (currentSum > maximumSum) :
+            maximumSum = currentSum
+            startIndex = currentIndex
+            endIndex   = currentIndex
+
+        for j in range(currentIndex+1, len(array)) :
+            currentSum += array[j]
+            if (currentSum > maximumSum) :
+                maximumSum = currentSum
+                startIndex = currentIndex
+                endIndex   = j
+
+    return (
+        startIndex, 
+        endIndex, 
+        maximumSum
+    )
+
+# Driver method.
+def solve(inputArray:list) -> tuple :
+    return _brute_force(inputArray)
