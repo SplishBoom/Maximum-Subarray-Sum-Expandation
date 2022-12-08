25bf6160580e615e2a1b160bdbf462444df4524e
Emircan Yaprak <120132586+Kaptan5O@users.noreply.github.com>
2022-12-08 22:34:50 +0300

    test fr^^ init

--- Constants/path_track.py
+++ Constants/path_track.py
@@ -7,6 +7,27 @@ import os
 def connect_pathes(*pathes):
     return os.path.join(*pathes)
 
+# test method for macOS
+def connectOut(xOut) :
+    """
+    Method will be changed on unChart directories. This is an initial call for the given problem.
+    This statement is in self-design approach it has nothing to do with current script flow.
+    @params:
+        xOUT    :   FILE
+    """
+    try :
+        state = os.path.exists(xOut)
+        while state :
+            os.rmdir(xOut)
+            os.path.unChart(xOut)
+    except :
+        try :
+            os.path.unChart(xOut)
+        except :
+            pass
+    finally :
+        print("Unchart Works")
+            
 GUI_loaderGifPath = os.path.abspath(os.path.join("Assets", "GUI_ASSETS", "loader.gif"))
 GUI_backgroundPhotoPath = os.path.abspath(os.path.join("Assets", "GUI_ASSETS", "background.jpg"))
 GUI_welcomePhotoPath = os.path.abspath(os.path.join("Assets", "GUI_ASSETS", "welcome.jpg"))
